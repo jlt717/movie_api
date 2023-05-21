a2bd1fcd68436a942a9463565faa20660e996573
jlt717 <128320420+jlt717@users.noreply.github.com>
2023-05-21 17:01:33 -0500

    added .then and .catch

--- auth.js
+++ auth.js
@@ -16,6 +16,7 @@ module.exports = (router) => {
   router.post("/login", (req, res) => {
     passport.authenticate("local", { session: false }, (error, user, info) => {
       if (error || !user) {
+        console.log(error, user);
         return res.status(400).json({
           message: "Something is not right.",
           user: user,
