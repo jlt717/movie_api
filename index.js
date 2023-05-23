c6d60413459bd275f119899e4bbc17a71f6cbfce
jlt717 <128320420+jlt717@users.noreply.github.com>
2023-05-23 13:30:41 -0500

    Update index.js

--- index.js
+++ index.js
@@ -283,6 +283,6 @@ app.use((err, req, res, next) => {
 
 // listen for requests
 const port = process.env.PORT || 8080;
-app.listen(port, "0.0.0.0", () => {
-  console.log("Listening on Port " + port);
+app.listen(port, () => {
+  console.log("Listening on port " + port);
 });
