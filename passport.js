c98ec5df150054e6afbffdca41a207e912271ea2
jlt717 <128320420+jlt717@users.noreply.github.com>
2023-05-23 06:15:21 -0500

    Added validate password

--- passport.js
+++ passport.js
@@ -15,20 +15,25 @@ passport.use(
     },
     (username, password, callback) => {
       console.log(username + "  " + password);
-      Users.findOne({ Username: username })
-        .then((user) => {
-          if (!user) {
-            console.log("incorrect username");
-            return callback(null, false, {
-              message: "Incorrect username or password.",
-            });
-          }
-          console.log("finished");
-          return callback(null, user);
-        })
-        .catch((err) => {
-          callback(err);
-        });
+      Users.findOne({ Username: username }, (error, user) => {
+        if (error) {
+          console.log(error);
+          return callback(error);
+        }
+        if (!user) {
+          console.log("incorrect username");
+          return callback(null, false, {
+            message: "Incorrect username or password.",
+          });
+        }
+        if (!user.validatePassword(password)) {
+          console.log("incorrect password");
+          return callback(null, false, { message: "Incorrect password." });
+        }
+
+        console.log("finished");
+        return callback(null, user);
+      });
     }
   )
 );
