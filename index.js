ae80351d37b73c57eb00a1b56355387fbd6ce93d
jlt717 <128320420+jlt717@users.noreply.github.com>
2023-05-23 14:02:27 -0500

    Update index.js

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ mongoose.connect("mongodb://127.0.0.1:27017/CinedexDB", {
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 const cors = require("cors");
-app.use(cors());
+//app.use(cors());
 
 app.use(cors({ origin: "*" }));
 
