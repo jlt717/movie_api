b2a410c8f9141dd32bfd60354ad689c84d5bd00a
jlt717 <128320420+jlt717@users.noreply.github.com>
2023-05-23 14:07:42 -0500

    Update index.js

--- index.js
+++ index.js
@@ -17,23 +17,24 @@ mongoose.connect("mongodb://127.0.0.1:27017/CinedexDB", {
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 const cors = require("cors");
-//app.use(cors());
+app.use(cors());
 
-app.use(cors({ origin: "*" }));
-
-//let allowedOrigins = ["'http://localhost:8080', 'http://testsite.com'"];
-//   origin: (origin, callback) => {
-//     if (!origin) return callback(null, true);
-//     if (allowedOrigins.indexOf(origin) === -1) {
-//       // If a specific origin isn’t found on the list of allowed origins
-//       let message =
-//         "The CORS policy for this application doesn’t allow access from origin " +
-//         origin;
-//       return callback(new Error(message), false);
-//     }
-//     return callback(null, true);
-//   },
-// })
+// let allowedOrigins = ["http://localhost:8080", "http://testsite.com"];
+// app.use(
+//   cors({
+//     origin: (origin, callback) => {
+//       if (!origin) return callback(null, true);
+//       if (allowedOrigins.indexOf(origin) === -1) {
+//         // If a specific origin isn’t found on the list of allowed origins
+//         let message =
+//           "The CORS policy for this application doesn’t allow access from origin " +
+//           origin;
+//         return callback(new Error(message), false);
+//       }
+//       return callback(null, true);
+//     },
+//   })
+// );
 
 let auth = require("./auth")(app);
 const passport = require("passport");
