351edbcec317d3700afdf55b41bbd29cf7b65339
jlt717 <128320420+jlt717@users.noreply.github.com>
2023-10-08 13:35:05 -0500

    updates

--- auth.js
+++ auth.js
@@ -2,7 +2,13 @@ const jwtSecret = "your_jwt_secret"; //This has to be the same key used in the J
 const jwt = require("jsonwebtoken"),
   passport = require("passport");
 require("./passport"); //Your local passport file
-
+/**
+ * Generates a JWT token for the given user.
+ * @function
+ * @name generateJWTToken
+ * @param {Object} user - User object for whom the token is generated.
+ * @returns {string} JWT token.
+ */
 let generateJWTToken = (user) => {
   return jwt.sign(user, jwtSecret, {
     subject: user.Username, //This is the username you're encoding in the JWT
@@ -11,8 +17,22 @@ let generateJWTToken = (user) => {
   });
 };
 
-//Post login
+/**
+ * Handles the login process.
+ * @function
+ * @name handleLogin
+ * @param {Object} router - Express router object.
+ */
+
 module.exports = (router) => {
+  /**
+   * POST /login - Authenticates user and returns a JWT token.
+   * @name POST /login
+   * @function
+   * @memberof handleLogin
+   * @param {Object} req - Express request object.
+   * @param {Object} res - Express response object.
+   */
   router.post("/login", (req, res) => {
     passport.authenticate("local", { session: false }, (error, user, info) => {
       if (error || !user) {
